5c78258d7ea62924fe8be839533eb92d454c4c64
cominggithob <misscoming@gmail.com>
2013-06-16 22:04:35 +0800

    add turn angle image

--- NavierIOS/RouteNavigationViewController.m
+++ NavierIOS/RouteNavigationViewController.m
@@ -13,6 +13,140 @@
 @end
 
 @implementation RouteNavigationViewController
+{
+    bool isShowRouteGuideMenu;
+    int routeGuideMenuOffset;
+    UIView *routeGuideMenu;
+    
+    UIButton *routeGuideMenuLogoButton;
+    UIButton *routeGuideMenuHUDButton;
+    UIButton *routeGuideMenuSettingButton;
+    
+}
+
+-(void) addRouteGuideMenu
+{
+    UIView *subView;
+    isShowRouteGuideMenu = false;
+    
+    NSArray *xibContents = [[NSBundle mainBundle] loadNibNamed:@"RouteGuideMenu" owner:self options:nil];
+    
+    CGRect frame;
+
+    routeGuideMenu = [xibContents lastObject];
+    
+    routeGuideMenuOffset = 100;
+//    frame.origin.x = 0;
+//    frame.origin.y = 0;
+    //    frame.size = self.scrIcon.frame.size;
+//    frame.size.width = 277;
+//    frame.size.height = 127;
+    
+    logfns("RouteGuideMenu: frame: (%.0f, %.0f) (%.0f, %.0f), bounds: (%.0f, %.0f) (%.0f, %.0f)\n",
+           routeGuideMenu.frame.origin.x,
+           routeGuideMenu.frame.origin.y,
+           routeGuideMenu.frame.size.width,
+           routeGuideMenu.frame.size.height,
+           routeGuideMenu.bounds.origin.x,
+           routeGuideMenu.bounds.origin.y,
+           routeGuideMenu.bounds.size.width,
+           routeGuideMenu.bounds.size.height
+           );
+    
+//    routeGuideMenu.frame = frame;
+//    subView = [xibContents lastObject];
+
+    logfns("RouteGuideMenu: frame: (%.0f, %.0f) (%.0f, %.0f), bounds: (%.0f, %.0f) (%.0f, %.0f)\n",
+           routeGuideMenu.frame.origin.x,
+           routeGuideMenu.frame.origin.y,
+           routeGuideMenu.frame.size.width,
+           routeGuideMenu.frame.size.height,
+           routeGuideMenu.bounds.origin.x,
+           routeGuideMenu.bounds.origin.y,
+           routeGuideMenu.bounds.size.width,
+           routeGuideMenu.bounds.size.height
+           );
+
+//    routeGuideMenu = [subView.subviews lastObject];
+    
+
+
+    routeGuideMenuLogoButton        = (UIButton *)[routeGuideMenu viewWithTag:1];
+    routeGuideMenuHUDButton         = (UIButton *)[routeGuideMenu viewWithTag:2];
+    routeGuideMenuSettingButton     = (UIButton *)[routeGuideMenu viewWithTag:3];
+
+    logo(routeGuideMenuLogoButton);
+    
+    [routeGuideMenuLogoButton addTarget:self
+                               action:@selector(pressLogoButton:)
+                     forControlEvents:UIControlEventTouchUpInside];
+    
+    [routeGuideMenuHUDButton addTarget:self
+                             action:@selector(pressHUDButton:)
+                   forControlEvents:UIControlEventTouchUpInside];
+    
+    [routeGuideMenuSettingButton addTarget:self
+                                 action:@selector(pressSettingButton:)
+                       forControlEvents:UIControlEventTouchUpInside];
+   
+    [routeGuideMenu setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    routeGuideMenu.hidden = true;
+    [self.view addSubview:routeGuideMenu];
+  
+    routeGuideMenu.layer.borderColor    = [UIColor whiteColor].CGColor;
+    routeGuideMenu.layer.borderWidth    = 3.0f;
+    routeGuideMenu.layer.cornerRadius   = 10;
+    routeGuideMenu.layer.masksToBounds  = YES;
+    
+    NSDictionary *views = NSDictionaryOfVariableBindings(routeGuideMenu);
+    UIView *contentView = self.view;
+
+    [contentView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"[routeGuideMenu(277)]"
+                                             options:NSLayoutFormatAlignAllCenterX
+                                             metrics:nil
+                                               views:views]];
+    
+    [contentView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[routeGuideMenu(127)]"
+                                             options:NSLayoutFormatAlignAllCenterY
+                                             metrics:nil
+                                               views:views]];
+#if 0
+
+    [contentView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[routeGuideMenu]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+    
+    [contentView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[routeGuideMenu]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+#endif
+
+
+    
+    
+//    [_window visualizeConstraints:[contentView constraints]];
+}
+
+-(void) hideRouteGuideMenu
+{
+    if (isShowRouteGuideMenu)
+    {
+        routeGuideMenu.hidden = true;
+        isShowRouteGuideMenu = false;
+    }
+}
+
+-(void) initSelf
+{
+    isShowRouteGuideMenu = false;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -50,6 +184,23 @@
     [self.guideRouteUIView triggerLocationUpdate];
 }
 
+-(IBAction) pressLogoButton:(id) sender
+{
+    [self hideRouteGuideMenu];
+    [self dismissModalViewControllerAnimated:true];
+}
+
+-(IBAction) pressHUDButton:(id) sender
+{
+    [self hideRouteGuideMenu];
+    [self.guideRouteUIView setHUD];
+}
+
+-(IBAction) pressSettingButton:(id) sender
+{
+    [self hideRouteGuideMenu];    
+}
+
 -(void) startRouteNavigationFrom:(Place*) startPlace To:(Place*) endPlace
 {
 
@@ -64,6 +215,7 @@
     [super viewDidLoad];
     logfn();
     logo(self.guideRouteUIView);
+    [self addRouteGuideMenu];
     //    locationSimulator.delegate = (GuideRouteUIView*)self.view;
     //    [locationSimulator start];
     
@@ -93,9 +245,38 @@
     
 }
 
+-(void) showRouteGuideMenu
+{
+    if (!isShowRouteGuideMenu)
+    {
+        routeGuideMenu.hidden = false;
+        isShowRouteGuideMenu = true;
+    }
+}
+
 - (IBAction)tagAction:(id)sender
 {
+    
+    logfns("RouteGuideMenu: frame: (%.0f, %.0f) (%.0f, %.0f), bounds: (%.0f, %.0f) (%.0f, %.0f)\n",
+           routeGuideMenu.frame.origin.x,
+           routeGuideMenu.frame.origin.y,
+           routeGuideMenu.frame.size.width,
+           routeGuideMenu.frame.size.height,
+           routeGuideMenu.bounds.origin.x,
+           routeGuideMenu.bounds.origin.y,
+           routeGuideMenu.bounds.size.width,
+           routeGuideMenu.bounds.size.height
+           );
+    
     logfn();
+    if (isShowRouteGuideMenu)
+    {
+        [self hideRouteGuideMenu];
+    }
+    else
+    {
+        [self showRouteGuideMenu];
+    }
 }
 
 - (void)viewDidUnload {
